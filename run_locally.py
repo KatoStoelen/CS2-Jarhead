c9c26f43eafab7be55f1a067bd4fb8661352664d
Geir Ole H. Stevning <gostevning@gmail.com>
2018-10-15 22:56:45 +0200

    #1: Create replays folder for local run recordings

--- run_locally.py
+++ run_locally.py
@@ -1,9 +1,16 @@
-import json
+import json, time, os
 
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 
 from bot import MyBot
+
+
+def assure_path_exists(path):
+    dir = os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
 
 def main():
     with open("botinfo.json") as f:
@@ -11,10 +18,15 @@ def main():
 
     race = Race[info["race"]]
 
+    milliseconds = int(round(time.time() * 1000))
+    assure_path_exists("results/")
+    replay_file = "replays/" + str(milliseconds) + ".SC2Replay"
+
     run_game(maps.get("Abyssal Reef LE"), [
         Bot(race, MyBot()),
         Computer(Race.Random, Difficulty.Medium)
-    ], realtime=False, step_time_limit=2.0, game_time_limit=(60*20), save_replay_as="test.SC2Replay")
+    ], realtime=False, step_time_limit=2.0, game_time_limit=(60*20), save_replay_as=replay_file)
+
 
 if __name__ == '__main__':
     main()
