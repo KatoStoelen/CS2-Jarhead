44ad88941c70127e6fa6f7da372ee3ce7579ef04
Kato Stoelen <kato.stoelen@gmail.com>
2018-11-16 17:51:11 +0100

    Formatting of main.py

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,7 @@ import sc2
 from sc2.ids.unit_typeid import UnitTypeId
 from .logic import Builder, Trainer, UnitController
 
+
 class MyBot(sc2.BotAI):
     def __init__(self):
         self.builder = Builder()
@@ -16,4 +17,3 @@ class MyBot(sc2.BotAI):
         await self.unitController.on_step(self)
         await self.trainer.on_step(self)
         await self.builder.on_step(self)
-
