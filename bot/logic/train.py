ff32f93e253cd474954251c9c97ee2e5a2bcf5f6
Rosita Høybakken <rosita95@gmail.com>
2018-11-17 11:21:11 +0100

    train base marines

--- bot/logic/train.py
+++ bot/logic/train.py
@@ -6,6 +6,8 @@ class Trainer:
     async def on_step(self, bot: sc2.BotAI, iteration: int):
         await self.__train_scv(bot)
         await self.__train_marines(bot)
+        await self.__train_base_marines(bot)
+        #await self.__train_marauder(bot)
 
     async def __train_scv(self, bot: sc2.BotAI):
         # train SCVs
@@ -14,8 +16,19 @@ class Trainer:
 
     async def __train_marines(self, bot: sc2.BotAI):
         # train marines
-        for rax in bot.units(UnitTypeId.BARRACKS).ready.noqueue:
+        for rax in bot.units(UnitTypeId.BARRACKS).closer_than(25, bot.game_info.map_center.towards(bot.enemy_start_locations[0],25)).ready.noqueue:
             if not bot.can_afford(UnitTypeId.MARINE):
                 break
-            print("Training marine")
             await bot.do(rax.train(UnitTypeId.MARINE))
+    
+    async def __train_base_marines(self, bot: sc2.BotAI):
+        base_bunkers = bot.units(UnitTypeId.BUNKER).closer_than(15, bot.main_base_ramp.top_center)
+        for bunker in base_bunkers:
+            if bunker.cargo_used < bunker.cargo_max:
+                for rax in bot.units(UnitTypeId.BARRACKS).closer_than(15, bot.main_base_ramp.top_center).ready.noqueue:
+                    if not bot.can_afford(UnitTypeId.MARINE):
+                        return
+                    await bot.do(rax.train(UnitTypeId.MARINE))
+
+
+
